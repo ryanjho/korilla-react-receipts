85244292332e3ce2fd62741c13dececa6cde3d96
Ryan Ho <ryanjho90@gmail.com>
2020-07-31 02:21:08 +0800

    Updates

---
+++ precache-manifest.41276014778de5f1545a2eadcb6775c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2c539c9cd47086aa42a411984773536",
+    "url": "/korilla-react-receipts/index.html"
+  },
+  {
+    "revision": "f73265486119218198f8",
+    "url": "/korilla-react-receipts/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a79137e27d312196f896",
+    "url": "/korilla-react-receipts/static/js/2.484949f5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/korilla-react-receipts/static/js/2.484949f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f73265486119218198f8",
+    "url": "/korilla-react-receipts/static/js/main.4d7cfe8d.chunk.js"
+  },
+  {
+    "revision": "0ed70095b6fa23b35216",
+    "url": "/korilla-react-receipts/static/js/runtime-main.a0bdf3a3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/korilla-react-receipts/static/media/logo.5d5d9eef.svg"
+  }
+]);
